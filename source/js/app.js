7ff63931e1a72fe2f38e2db4f424be5c110101ac
Tatyana Serova <s9t711@yandex.ru>
2017-01-12 11:35:38 +0300

    third

--- source/js/app.js
+++ source/js/app.js
@@ -1,10 +1,14 @@
 'use strict';
 
+$(function(){
+  $('#front').on('click', function(e) {
+    e.preventDefault();
+    $('.flipper').css('transform', 'rotateY(180deg');
+  });
+  $('#back').on('click', function(e) {
+    e.preventDefault();
+    $('.flipper').css('transform', 'rotateY(0deg');
+  });
+});
 
-let ready = () => {
-  console.log('Page ready');
 
-};
-
-
-document.addEventListener('DOMContentLoaded', ready);

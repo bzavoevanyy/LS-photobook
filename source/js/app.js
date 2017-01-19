b174c730e96ba5df806c832e20a4a2b43d7f01db
Tatyana Serova <salsa777@yandex.ru>
2017-01-19 22:22:37 +0300

    pug

--- source/js/app.js
+++ source/js/app.js
@@ -3,8 +3,30 @@
 $(function(){
   $('#front').on('click', function(e) {
     e.preventDefault();
-    $('.flipper').css('transform', 'rotateY(180deg');
+      $('.flipper').css('transform', 'rotateY(180deg');
   });
 });
 
+$(document).ready(function(){
+    $('#toggle').click(function() {
+        $(this).toggleClass('active');
+        $('.edit__down').toggleClass('open');
+        $('.edit__header').css('display', 'block');
+    });
 
+    $('#exit').click(function() {
+        $(this).toggleClass('active');
+        $('.edit__down').removeClass('open');
+        $('.edit__header').css('display', 'none');
+    });
+});
+
+//scrooll
+$(window).scroll(function(){
+    if ($(window).scrollTop() > 300) {
+        $('.header-fixed').addClass('top');
+    }
+    else {
+        $('.header-fixed').removeClass('top');
+    }
+});
